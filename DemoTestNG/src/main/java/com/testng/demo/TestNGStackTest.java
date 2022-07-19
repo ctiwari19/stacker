53ec65f84cf83c0c7e7246258e1e4065648809c9
hp-u <hp-u@DESKTOP-OHAED0Q>
2022-07-19 11:43:56 +0530

    it is sub

---
+++ DemoTestNG/src/main/java/com/testng/demo/TestNGStackTest.java
@@ -0,0 +1,14 @@
+package com.testng.demo;
+
+import org.testng.annotations.Test;
+
+public class TestNGStackTest
+{
+@Test
+
+public void TestNGStack()
+{
+	System.out.println("Stakkerrs");
+	
+	}
+}
---
+++ DemoTestNG/src/main/java/com/testng/demo/TestNgDemoTest.java
@@ -0,0 +1,19 @@
+package com.testng.demo;
+
+import org.testng.annotations.Test;
+
+
+public class TestNgDemoTest {
+@Test
+	public void  TestNgDemo()
+	{
+String url =  System.getProperty(" URL" );
+String browser =  System.getProperty(" BROWSER" );
+String username =  System.getProperty(" USERNAME" );
+String password =  System.getProperty(" PASSWORD" );
+		System.out.println("We Want ID card");
+		
+	}
+
+
+}
